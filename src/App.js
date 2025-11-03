2f109f83b31a8e3a1df8963afd87849c50e63cff
shristi58 <kumarishristi22004@gmail.com>
2025-11-03 20:49:33 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import "./App.css"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to My Dockerized React App 🚀</h1>
+      <p>This app is built using a multi-stage Docker build.</p>
+    </div>
+  )
+}
+
+export default App
